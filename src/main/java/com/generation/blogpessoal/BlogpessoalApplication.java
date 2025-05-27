f1f6c5f5d763e7001016e1e231eb8d084322611e
rafaela_giometti <rafaela_giometti@hotmail.com>
2025-05-27 19:50:06 -0300

    :sparkles: Deploy do projeto Blog Pessoal

--- src/main/java/com/generation/blogpessoal/BlogpessoalApplication.java
+++ src/main/java/com/generation/blogpessoal/BlogpessoalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogpessoalRafaApplication {
+public class BlogpessoalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogpessoalRafaApplication.class, args);
+		SpringApplication.run(BlogpessoalApplication.class, args);
 	}
 
 }
--- src/test/java/com/generation/blogpessoal/BlogpessoalApplicationTests.java
+++ src/test/java/com/generation/blogpessoal/BlogpessoalApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BlogpessoalRafaApplicationTests {
+class BlogpessoalApplicationTests {
 
 	@Test
 	void contextLoads() {
